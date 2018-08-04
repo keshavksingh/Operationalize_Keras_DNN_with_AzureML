0591a291fdd3237e4896e4f2e776d47fc7f3b9f3
keshavksingh <41631964+keshavksingh@users.noreply.github.com>
2018-08-04 20:16:55 +0530

    Add files via upload

--- score.py
+++ score.py
@@ -4,6 +4,9 @@ from azureml.api.schema.dataTypes import DataTypes
 from azureml.api.schema.sampleDefinition import SampleDefinition
 from azureml.api.realtime.services import generate_schema
 from azureml.assets import get_local_path
+import numpy as np
+import pandas as pd
+from keras.models import load_model
 
 # Prepare the web service definition by authoring
 # init() and run() functions. Test the functions
@@ -16,43 +19,79 @@ def init():
     # local_path = get_local_path('mymodel.model.link')
     
     # Load model using appropriate library and function
-    global model
+    #global model
     # model = model_load_function(local_path)
-    model = 42
+    #from sklearn.externals import joblib
+    global model
+    #model=joblib.load('PredictPurchase_model.h5')
+    model= load_model("PredictPurchase_model.h5")
 
 def run(input_df):
     import json
     
     # Predict using appropriate functions
     # prediction = model.predict(input_df)
+    y_pred = model.predict_classes(input_df)
+    predictions=pd.DataFrame(data=y_pred,columns=['PredictedProduct'])
+    predictions['PredictedProduct']= predictions['PredictedProduct'].apply(lambda x: 'Product_C' if x ==2 else ('Product_B' if x==1 else 'Product_A'))
+    return json.dumps(str(predictions['PredictedProduct'][0]))
 
-    prediction = "%s %d" % (str(input_df), model)
-    return json.dumps(str(prediction))
+    #prediction = "%s %d" % (str(input_df), model)
+    #return json.dumps(str(prediction))
 
-def generate_api_schema():
-    import os
-    print("create schema")
-    sample_input = "sample data text"
-    inputs = {"input_df": SampleDefinition(DataTypes.STANDARD, sample_input)}
-    os.makedirs('outputs', exist_ok=True)
-    print(generate_schema(inputs=inputs, filepath="outputs/schema.json", run_func=run))
+#def generate_api_schema():
+#    import os
+#    import pandas as pd
+#    columns=['TimeSpentOnWeb','TimeSpentOnProductPage']
+#    sample_input = pd.DataFrame(data=[(5.1,3.5)],columns=columns)
+#    print("create schema")
+#    #sample_input = "sample data text"
+#    inputs = {"input_df": SampleDefinition(DataTypes.PANDAS, sample_input)}
+#    os.makedirs('outputs', exist_ok=True)
+#    print(generate_schema(inputs=inputs, filepath="service-schema.json", run_func=run))
 
 # Implement test code to run in IDE or Azure ML Workbench
 if __name__ == '__main__':
     # Import the logger only for Workbench runs
     from azureml.logging import get_azureml_logger
-
-    logger = get_azureml_logger()
-
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--generate', action='store_true', help='Generate Schema')
-    args = parser.parse_args()
-
-    if args.generate:
-        generate_api_schema()
+    import numpy as np
+    import pandas as pd
+    columns=['TimeSpentOnWeb','TimeSpentOnProductPage']
+    sample_input = pd.DataFrame(data=[(5.1,3.5)],columns=columns)
 
     init()
-    input = "{}"
-    result = run(input)
+    #input = "{}"
+    result = run(sample_input)
+    print("The predicted Product which will be Purchased is -",str(result))
+
+    ##Generating Schema
+    inputs = {"input_df": SampleDefinition(DataTypes.PANDAS, sample_input)}
+    print(generate_schema(inputs=inputs, filepath="service-schema.json", run_func=run))
+
+    logger = get_azureml_logger()
     logger.log("Result",result)
+
+    ## To use your existing model management account
+    ## az ml account modelmanagement set -n dsazmlexpmodelmgmt -g datascience
+    ## Setup a NEW Model management
+    ##az ml account modelmanagement create -l [Azure region, e.g. eastus2] -n [your account name] -g [resource group name] --sku-instances [number of instances, e.g. 1] --sku-name [Pricing tier for example S1]
+    ## Next setup your local or cluster mode deployment
+    ## az ml env setup -l eastus2 -n dslocal -g datascience
+    ## az ml env show -l eastus2 -n dslocal -g datascience  
+    ## az ml env set -n dslocal -g datascience  
+    ## Alternatively For setting up ACS-Kubernetes Cluster mode, create 3 agent  (default)
+    ## az ml env setup -l eastus2 -n dscluster -g datascience --cluster --agent-count-3
+    ## az ml env set -n dscluster -g datascience  
+    ## Next once the envoriment is set, the bext command in one step will register model, create manifest, create image and setup service
+    ## command - az ml service create realtime -f score.py --model-file PredictPurchase_model.h5 -s service-schema.json -n productapp -r python--collect-model-data true -c aml_config\conda_dependencies.yml
+    ## Alternatively you can do this step by step and keep passing each Ids to the next step 
+    ##az ml service create realtime -n productapp -r python --collect-model-data true --image-id 8f14e874-4473-40d7-aca2-f2a651d24c48
+    ## Example:
+    ##az ml model register --model model.pkl --name <<GiveYourModelName>> (This will return a ModelId, next we build a Manifest)
+    ##az ml manifest create --manifest-name <<GiveAManifestName>> -f score.py -r python -s service_schema.json -c aml_config/conda_dependencies.yml --model-id <ModelId> (Next We Need to Create Image)
+    ##az ml image create --image-name <<GiveAnImageName>> -f score.py -r python -s service_schema.json -c aml_config/conda_dependencies.yml --manifest-id <<ManifestId from the previous step>>
+    ##Az ml service create realtime -n <<GiveServiceAName>> -r python --collect-model-data true --image-id <<Image Id from Previous Step>>
+    ## Now using the Service
+    ## az ml service run realtime -i productapp -d "{\"input_df\": [{\"TimeSpentOnWeb\": 5.1, \"TimeSpentOnProductPage\": 3.5}]}"
+    ##az ml service logs realtime -i <<ServiceName>> (For getting the service telemetry)
+    
--- train.py
+++ train.py
@@ -4,8 +4,55 @@
 import sys
 import os
 import argparse
-
 from azureml.logging import get_azureml_logger
+from sklearn.model_selection import train_test_split
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop,Adam
+from keras.models import Model
+import numpy as np
+import pandas as pd
+train = pd.read_csv('ProductPurchase.csv')
+train.head()
+train.columns
+X_train=train[['TimeSpentOnWeb','TimeSpentOnProductPage']]
+Y_train=train[['ProductPurchased']]
+Col_To_Transform=['ProductPurchased']
+Y_With_Dummies=pd.get_dummies(columns=Col_To_Transform,data=Y_train)
+X_train = X_train[['TimeSpentOnWeb','TimeSpentOnProductPage']].astype(float)
+Y_train = Y_With_Dummies[['ProductPurchased_A','ProductPurchased_B','ProductPurchased_C']].astype(float)
+
+XX_train, XX_test, yy_train, yy_test = train_test_split(X_train,Y_train,test_size=0.25, random_state=11)
+
+model = Sequential()
+model.add(Dense(16, activation='relu', input_shape=(2,)))
+model.add(Dense(8, activation='relu', input_shape=(16,)))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+history = model.fit(XX_train, yy_train,
+                    epochs=1000,batch_size=5,
+                    verbose=1,
+                    validation_data=(XX_test, yy_test))
+score = model.evaluate(XX_test, yy_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+from keras.models import load_model
+model.save("PredictPurchase_model.h5")
+m = load_model("PredictPurchase_model.h5")
+
+preddf=pd.read_csv('PredictProductPurchase.csv')
+y_pred = m.predict_classes(preddf,verbose=1)
+
+y_pred = model.predict_classes(preddf,verbose=1)
+y_pred
+predictions=pd.DataFrame(data=y_pred,columns=['PredictedProduct'])
+predictions['PredictedProduct']= predictions['PredictedProduct'].apply(lambda x: 'Product_C' if x ==2 else ('Product_B' if x==1 else 'Product_A'))
+print(predictions)
 
 # initialize the logger
 logger = get_azureml_logger()
